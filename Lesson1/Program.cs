6808517c17ae63518ebfc8858b053abceff370bb
AlimovPhil <retrom.rus@gmail.com>
2022-04-28 23:27:20 +0300

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime CrntDate = DateTime.Now; //Объявляем метод, который будет выводить текущее время
+            Console.Write("Пожалуйста, введите ваше имя: "); //Запрос имени у пользователя
+            string Name = Console.ReadLine(); //Записываем введенное имя
+            Console.WriteLine("**************************************************"); //Символы для лучшей читаемости последующего результата
+            Console.WriteLine($"Здравствуйте, {Name}, сегодня {CrntDate.ToString("dddd, dd MMMM yyyy")}"); //Выводим полученное имя и сообщаем дату
+
+            Console.ReadLine();
+        }
+    }
+}
