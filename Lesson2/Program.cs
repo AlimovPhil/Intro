4adb9afa102a83600de672f41911e4b5a15145bf
AlimovPhil <retrom.rus@gmail.com>
2022-05-16 12:10:57 +0300

    Работаю над заданием 4

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -45,23 +45,23 @@ namespace Lesson2
         /// <summary>
         /// Задание 1. Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру
         /// </summary>
-        
         static void Task1()
         {
+
             Console.Write("Введите минимальную температуру сегодня: ");
             float min = float.Parse(Console.ReadLine());
-            
+
             Console.Write("Введите максимальную температуру сегодня: ");
             float max = float.Parse(Console.ReadLine());
 
             float average = (min + max) / 2; //формула вычисления средней темп
-            
+
             Console.WriteLine($"Среднесуточная температура: {average}");
         }
 
 
         /// <summary>
-        /// Задание 1, 2, 5. Пока не понятно, как из отдельного метода Task1 вытащить значение average
+        /// Задание 1, 2, 5. Пока не понятно, как вытаскивать данные, нужно учить Поля и get\set
         /// </summary>
         static void Task125()
         {
@@ -71,10 +71,10 @@ namespace Lesson2
 
             Console.Write("Введите максимальную температуру сегодня: ");
             float max = float.Parse(Console.ReadLine());
-
             float average = (min + max) / 2; //формула вычисления средней темп
 
             Console.WriteLine($"Среднесуточная температура: {average}");
+
 
             //Задание 2. Запрос у пользователя номера месяца.
             Console.Write("Введите порядковый номер текущего месяца: ");
@@ -98,7 +98,7 @@ namespace Lesson2
                     break;
 
                 case 4:
-                    Console.WriteLine("На дворе Апрель"); //если не нужно сохранять значение monthName
+                    Console.WriteLine("На дворе Апрель"); //если не нужно сохранять значение monthName, 3 кэйса описал так нарочно.
                     break;
 
                 case 5:
@@ -139,7 +139,9 @@ namespace Lesson2
                     Console.Write($"На дворе {monthName}");
                     break;
             }
-            // Задание 5. Дождливая ли зима? Вывод результата проверки через созданый метод. 
+
+
+            // Задание 5. Дождливая ли зима? Вывод результата проверки через созданый метод ниже. 
             string weather = RainyWeather(average, monthNum);
             Console.WriteLine(weather);
         }
@@ -151,28 +153,27 @@ namespace Lesson2
         /// <param name="monthNum">Номер текущего месяца</param>
         /// <returns></returns>
         static string RainyWeather(float average, int monthNum)
-        { 
+        {
             if ((average > 0) && (monthNum == 1) || (monthNum == 2) || (monthNum == 12))
             {
-                return ", за окном дождливая зима";
+                return ", за окном дождливая зима...";
             }
             else
             {
                 return "";
             }
         }
-        
-            
+
+
         /// <summary>
-        /// Задание 3. Определить, является ли введённое пользователем число чётным
+        /// Задание 3. Определить, является ли введённое пользователем число чётным.
         /// </summary>
         static void Task3()
         {
             Console.Write("Введите число: ");
             int a = int.Parse(Console.ReadLine());
-            int b = 2;
-
-            if (a % b == 0)
+            bool chet = a % 2 == 0;
+            if (chet)
             {
                 Console.WriteLine("Число четное");
             }
@@ -183,14 +184,66 @@ namespace Lesson2
 
         }
 
+        /// <summary>
+        /// Задание 4. Вывод чека в консоль.
+        /// </summary>
+        static void Task4()
+        {
+            DateTime Date = new DateTime(2022, 04, 22, 19, 11, 26);
+            string item1 = "Number 9 large";
+            string item2 = "Number 6 with extra dip";
+            string item3 = "Number 45";
+            string item4 = "Large soda";
+            float price1 = 150.25F;
+            float price2 = 102F;
+            float price3 = 73.30F;
+            float price4 = 54.75F;
+            short amount1 = 1;
+            short amount2 = 1;
+            short amount3 = 2;
+            short amount4 = 4;
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("CLUCKIN' BELL");
+            Console.WriteLine("Кассовый чек");
+
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуй, user!");
+            Console.WriteLine("Здравствуйте, Станислав!");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Меню: ");
+            Console.WriteLine("1. Задания 1, 2, 5");
+            Console.WriteLine("2. Задание 3");
+            //Console.WriteLine("3. Задание 4");
+            Console.WriteLine("4. Выход из приложения");
+            Console.WriteLine("----------------------------------------------------");
+            Console.Write("Выберите пункт меню для запуска задания: ");
+
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 4:
+                    Console.WriteLine("----------------------------------------------------");
+                    Console.WriteLine("...Завершение работы приложения...");
+                    break;
+                case 1:
+                    Console.WriteLine("----------------------------------------------------");
+                    Task125();
+                    break;
+                case 2:
+                    Console.WriteLine("----------------------------------------------------");
+                    Task3();
+                    break;
+                //case 3:
+                //    Console.WriteLine("----------------------------------------------------");
+                //    Task4();
+                //    break;
+
+            }
+
             //Task1();
-            //Task2();
-            Task125();
-            Task3();
             Console.ReadKey();
         }
     }
