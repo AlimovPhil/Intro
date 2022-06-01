62c2c59f0641287707087ca2d344ce516144ef9a
AlimovPhil <retrom.rus@gmail.com>
2022-06-01 14:58:43 +0300

    Задания 4 и 5 не решены

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
 
-/* 
- 3. Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный
-файл.
-4. (*) Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с
-рекурсией и без.
-5. (*) Список задач(ToDo-list):
-● написать приложение для ввода списка задач;
-● задачу описать классом ToDo с полями Title и IsDone;
-● на старте, если есть файл tasks.json/xml/bin (выбрать формат), загрузить
-из него массив имеющихся задач и вывести их на экран;
-● если задача выполнена, вывести перед её названием строку «[x]»;
-● вывести порядковый номер для каждой задачи;
-● при вводе пользователем порядкового номера задачи отметить задачу с этим
-порядковым номером как выполненную;
-● записать актуальный массив задач в файл tasks.json/xml/bin.*/
-
 
 namespace Lesson5
 {
@@ -89,6 +73,146 @@ namespace Lesson5
             //Console.WriteLine(time.ToLongTimeString());
         }
 
+        /// <summary>
+        ///  3. Ввести с клавиатуры произвольный набор чисел(0...255) и записать их в бинарный файл.
+        /// </summary>
+        static string[] separator = { ",", ".", " ", "?", "!", ";", ":" };
+        static byte[] StringToBytes(string input) //Метод преобразования строки в байты
+        {
+            string[] chars = input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            byte[] bytes = Array.ConvertAll(chars, byte.Parse);
+            return bytes;
+        }
+
+        static void Task3()
+        {
+            string fileName = "bytes.bin";
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255 через пробел");
+            string input = Console.ReadLine();
+            byte[] bytes = StringToBytes(input);
+            File.WriteAllBytes(fileName, bytes);
+            byte[] newData;
+            using (var br = new BinaryReader(File.OpenRead(fileName)))
+            {
+                newData = br.ReadBytes(bytes.Length);
+            }
+
+            Console.Write($"Операция завершена!\nВы записали в файл {fileName} следующие данные:\n");
+            for (var i = 0; i < newData.Length; i++)
+            {
+                Console.Write(newData[i] + " ");
+            }
+        }
+
+        /// <summary>
+        /// 4. (*) Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
+        /// </summary>
+        static void Task4()
+        {
+            //DirectoryInfo directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+            //Console.WriteLine("Full Name: {0}", directoryInfo.FullName);
+            //Console.WriteLine("Name: {0}", directoryInfo.Name);
+            //Console.WriteLine("Parent: {0}", directoryInfo.Parent);
+            //Console.WriteLine("Creation time: {0}", directoryInfo.CreationTime);
+            //Console.WriteLine("Attributes: {0}", directoryInfo.Attributes.ToString());
+            //Console.WriteLine("Root: {0}", directoryInfo.Root);
+
+            //Console.WriteLine(fileInfo.FullName);
+            //Console.WriteLine(fileInfo.Name);
+            //Console.WriteLine(fileInfo.CreationTime);
+            //Console.WriteLine(fileInfo.Extension);
+
+            //FileInfo[] fileInDir = directoryInfo.GetFiles(); //вывод всех файлов в заданной директории
+            //for (var i = 0; i < fileInDir.Length; i++)
+            //{
+            //    Console.WriteLine(fileInDir[i].Name);
+            //}
+            Console.WriteLine(".............................................");
+
+            Console.WriteLine("Введите путь в файловой системе, для записи в текстовый файл:");
+            //PrintDir01(new DirectoryInfo(Console.ReadLine()), "", true);
+            string path = "Task4.txt";
+            string str = PrintDir01(new DirectoryInfo(Console.ReadLine()), "", true);
+            File.WriteAllText(path, str);
+
+        }
+           
+        static string PrintDir01(DirectoryInfo dir, string intend, bool lastDir)
+        {
+            string str = dir.Name + intend;
+            Console.Write(intend);
+            intend += lastDir ? " " : "│ ";
+            Console.Write(intend = "  ");
+            Console.Write(lastDir ? "└─" : "├─");
+            
+            FileInfo[] fileInDir = dir.GetFiles();
+            Console.WriteLine(dir.Name);
+
+            for (var i = 0; i < fileInDir.Length; i++)
+            {
+                Console.WriteLine($"    │{intend}└─ {fileInDir[i].Name.ToString()}"); //тут схалтурил. Подскажите, что нужно было добавить в код, чтобы корректно все отрисовывалось
+            }
+           
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                str += $"  {PrintDir01(subDirs[i], intend, i == subDirs.Length - 1)}\n";
+            }           
+            return str;
+        }
+
+        //static void PrintDir(DirectoryInfo dir, string intend, bool lastDir)
+        //{
+        //    Console.Write(intend);
+        //    Console.Write(lastDir ? "└─" : "├─");
+        //    intend += lastDir ? " " : "│ ";
+        //    Console.WriteLine(dir.Name);
+        //    FileInfo[] fileInDir = dir.GetFiles();
+        //    for (var i = 0; i < fileInDir.Length; i++)
+        //    {
+        //        Console.WriteLine($".../{fileInDir[i].Name}");
+        //    }
+
+        //    DirectoryInfo[] subDirs = dir.GetDirectories(); 
+        //    for (int i = 0; i < subDirs.Length; i++)
+        //    {
+        //        PrintDir(subDirs[i], intend, i == subDirs.Length - 1);
+        //    }
+        //}
+
+        /// <summary>
+        /// 5. (*) Список задач(ToDo-list):
+        /// ● написать приложение для ввода списка задач;
+        /// ● задачу описать классом ToDo с полями Title и IsDone;
+        /// ● на старте, если есть файл tasks.json/xml/bin(выбрать формат), загрузить из него массив имеющихся задач и вывести их на экран;
+        /// ● если задача выполнена, вывести перед её названием строку «[x]»;
+        /// ● вывести порядковый номер для каждой задачи;
+        /// ● при вводе пользователем порядкового номера задачи отметить задачу с этим порядковым номером как выполненную;
+        /// ● записать актуальный массив задач в файл tasks.json/xml/bin.
+        /// </summary>
+        static void Task5()
+        {
+
+            Console.WriteLine("Введите название задачу для добавления: ");
+            string title = Console.ReadLine();
+            ToDo NewTask = new ToDo (title, false);
+            NewTask.AddTaskToTable();
+
+            
+            ToDo task1 = new ToDo("Купить молоко", false);
+            Console.WriteLine(task1.ShowFullTask());
+            Console.WriteLine(task1.TaskDone(1));
+            Console.WriteLine(task1.ShowFullTask());    
+
+            ToDo task2 = new ToDo("Погладить кота", false);
+            Console.WriteLine(task2.ShowTask());
+            
+            ToDo task3 = new ToDo("Захватить мир", false);
+            Console.WriteLine(task3.ShowTask());
+
+        }
+
         static void Main(string[] args)
         {
             Console.Title = "Lesson 4";
@@ -98,9 +222,10 @@ namespace Lesson5
                 Console.WriteLine("Список приложений: ");
                 Console.WriteLine("1. Ввод с клавиатуры и сохранение в файл");
                 Console.WriteLine("2. Дописать в файл Startup.txt текущее время");
-                Console.WriteLine("3. Определение времени года");
-                Console.WriteLine("4. Число Фибонначи");
-                Console.WriteLine("5. Выход из приложения");
+                Console.WriteLine("3. Запись бинарного файла");
+                Console.WriteLine("4*. Сохранение дерева каталогов и файлов в текстовый файл (с\\без рекурсии) (НЕ РЕШЕНО)");
+                Console.WriteLine("5*. ToDo-list (НЕ РЕШЕНО)");
+                Console.WriteLine("0. Выход из приложения");
                 Console.WriteLine("----------------------------------------------------");
                 Console.Write("Выберите пункт меню для запуска задания: ");
 
@@ -120,15 +245,20 @@ namespace Lesson5
                             break;
                         case 3:
                             Console.WriteLine("----------------------------------------------------");
-                            //Task3();
+                            Task3();
                             Console.ReadKey();
                             break;
                         case 4:
                             Console.WriteLine("----------------------------------------------------");
-                            //Task4();
+                            Task4();
                             Console.ReadKey();
                             break;
                         case 5:
+                            Console.WriteLine("----------------------------------------------------");
+                            Task5();
+                            Console.ReadKey();
+                            break;
+                        case 0:
                             Console.WriteLine("----------------------------------------------------");
                             Console.WriteLine("Завершение работы приложения...");
                             Console.ReadKey();
---
+++ Lesson5/ToDo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+5. (*) Список задач(ToDo-list):
+● написать приложение для ввода списка задач;
+● задачу описать классом ToDo с полями Title и IsDone;
+● на старте, если есть файл tasks.json/xml/bin (выбрать формат), загрузить из него массив имеющихся задач и вывести их на экран; -
+● если задача выполнена, вывести перед её названием строку «[x]»; 
+● вывести порядковый номер для каждой задачи;
+● при вводе пользователем порядкового номера задачи отметить задачу с этим порядковым номером как выполненную;
+● записать актуальный массив задач в файл tasks.json/xml/bin.*/
+namespace Lesson5
+{
+    public class ToDo
+    {
+        #region Fields
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Признак выполнения задачи
+        /// </summary>
+        public bool IsDone { get; set; }
+
+        #endregion
+
+        public List<string> Tasks = new List<string>();
+
+        public ToDo() //конструктор
+        {
+
+        }
+        /// <summary>
+        /// Создает заметку с параметрами
+        /// </summary>
+        /// <param name="Title">Название задачи</param>
+        /// <param name="IsDone">Признак выполнения</param>
+        public ToDo(string Title, bool IsDone)
+        {
+            this.Title = Title;
+            this.IsDone = IsDone;
+        }
+        
+        public void AddTaskToTable()
+        {
+            Tasks.Add(Title);
+        }
+        
+        public string ShowTask()
+        {
+            return Title;
+        }
+
+        public string ShowFullTask()
+        {
+            return Title + " " + IsDone.ToString();
+        }
+
+        public string TaskDone(int a)
+        {
+            if (a == 1)
+            {
+                this.IsDone = true;
+                return "[x]" + Title;
+            }
+            else return Title;
+            
+        }
+        
+
+    }
+}
