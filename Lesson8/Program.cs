036648732a38a71e3d180d5f7d93440e88243587
AlimovPhil <retrom.rus@gmail.com>
2022-06-05 11:11:27 +0300

    Started lessson8 solution. Added additional Library.

---
+++ Lesson8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson8.TestLib;
+
+namespace Lesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputHelpers.HwHeader(8, "Алимов Филипп Рашидович");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Phil_Library/OutputHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8.TestLib
+{
+    public class OutputHelpers
+    {
+        public static void HwHeader(int lesson, string name)
+        {
+            Console.WriteLine($"Домашняя работа. Урок {lesson}.\nВыполнил студент: {name}.");
+            Console.WriteLine("******************************************\n");
+        }
+    }
+}
